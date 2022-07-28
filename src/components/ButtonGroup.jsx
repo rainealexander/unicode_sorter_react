e300bb381c44d5d3163e12415caebb00822dc6ae
taadams1 <rainedoescode@gmail.com>
2022-07-28 12:49:58 -0500

    Minor button updates

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Button from './Button.jsx'
 
 const ButtonGroup = (props) => {
-  const defaultStyle = 'text-sm bg-white hover:bg-gray-500 hover:font-medium hover:text-white py-1 px-2 mt-2 border border-black rounded';
+  const defaultStyle = 'w-[10vw] min-w-fit text-sm bg-white hover:bg-gray-500 hover:font-medium hover:text-white py-1 px-2 mt-2 border border-black rounded';
   return (
     <div class='flex flex-col ml-3'>
-      <Button classes={defaultStyle} onClick={() => {}} buttonContent={'Ignore'} />
-      <Button classes={defaultStyle} onClick={() => {}} buttonContent={'Save'} />
-      <Button classes={defaultStyle} onClick={() => {}} buttonContent={'Emoji'} />
+      <Button classes={defaultStyle} handleClick={() => {}} buttonContent={'Ignore'} />
+      <Button classes={defaultStyle} handleClick={() => {}} buttonContent={'Save'} />
+      <Button classes={defaultStyle} handleClick={() => {}} buttonContent={'Emoji'} />
     </div>
   );
 };
